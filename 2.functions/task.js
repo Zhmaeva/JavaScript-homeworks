80e652dac32f785b517a04d567f37757f79cf8d9
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-05-16 22:25:47 +0300

    task_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,18 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
