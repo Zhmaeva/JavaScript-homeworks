5b38c1d5b3fc46de1977d115cc24067bacae4d27
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-05-16 14:47:10 +0300

    Update

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,9 +1,9 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+  const arr = [];
   
   // Вычисляем дискриминант.
-  let d = b ** 2 - 4 * a * c;  
+  const d = b ** 2 - 4 * a * c;  
   
   // Если дискриминант меньше нуля, то корней нет (пустой массив).
   if (d < 0) {
@@ -12,14 +12,14 @@ function solveEquation(a, b, c) {
 
   // Если дискриминант равен нулю, то корень один.
   if (d === 0) {
-    let x = -b / (2 * a);
+    const x = -b / (2 * a);
     arr.push(x);
   }
 
   // Если дискриминант больше нуля, то существует два решения уравнения.
   if (d > 0) {
-    let x1 = (-b + Math.sqrt(d)) / (2 * a);
-    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
 
@@ -27,9 +27,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentPerMonth = percent / 100 / 12;
-  let creditBody = amount - contribution;
-  let payment = creditBody * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
-  let totalAmount = payment * countMonths;
+  const percentPerMonth = percent / 100 / 12;
+  const creditBody = amount - contribution;
+  const payment = creditBody * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+  const totalAmount = payment * countMonths;
   return +totalAmount.toFixed(2);
 }
