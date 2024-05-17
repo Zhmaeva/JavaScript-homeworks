08ae1aed7265d518a5310fa146c51ff94a718a8f
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-05-17 16:54:52 +0300

    task_3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -73,5 +73,13 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for(let i = 0; i < arrOfArr.length; i++) {
+    const result = func(...arrOfArr[i]);
+    if(result > maxWorkerResult) {
+      maxWorkerResult = result;
+    }
+  }
 
+  return maxWorkerResult;
 }
