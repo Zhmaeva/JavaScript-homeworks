4aebd8a44fb4f4dfd3ce7fef7d90ae1e70cf8adf
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-05-17 16:29:16 +0300

    task_2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -18,19 +18,58 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  const sum = arr.reduce(function (resultSum, currentElement) {
+    return resultSum + currentElement
+  }, 0)
 
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  if(arr.length === 0) {
+    return 0;
+  }
 
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let difference = max - min;
+  
+  return difference;
 }
 
 function differenceEvenOddWorker(...arr) {
+  if(arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i= 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if(arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement += 1;
+    }
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
