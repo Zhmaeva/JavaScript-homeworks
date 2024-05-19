deb2377b6dff1d5dc864ae4bed519bae51ccd1ca
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-05-19 16:47:43 +0300

    Complete task_1, task_2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,48 @@
 function compareArrays(arr1, arr2) {
-  
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    const result = arr1.every((value, index) => value === arr2[index]);
+
+    return result;
 }
 
+/*
 function getUsersNamesInAgeRange(users, gender) {
-  
+    if(users.length === 0) {
+        return 0;
+    }
+    const usersWithTargetGender = users.filter(user => user.gender === gender);
+
+    if(usersWithTargetGender.length === 0) {
+        return 0;
+    }
+    
+    let numberOfPeople = 0;
+    const result = usersWithTargetGender.map(user => user.age).reduce(function(sum, current) {
+        numberOfPeople += 1;
+        return sum + current;
+    },0);
+
+    return result / numberOfPeople;
+}
+*/
+
+function getUsersNamesInAgeRange(users, gender) {
+    if(users.length === 0) {
+        return 0;
+    }
+    
+    let numberOfPeople = 0;
+
+    const result = users.reduce((acc, curr) => {
+        if(curr.gender !== gender) return acc;
+
+        numberOfPeople++;
+        return acc + curr.age;
+    }, 0)
+
+    if(numberOfPeople === 0) return 0;
+
+    return result / numberOfPeople;
 }
