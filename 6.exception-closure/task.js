eac721717a5094d7d34f3b0ca9e7e49b0a7fee8d
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-06-03 20:44:15 +0300

    task#2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -12,3 +12,41 @@ function validateCount(value) {
         return error;
     }
 }
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(a === 0 || b === 0 || c === 0) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        } else if(a > (b + c) || b > (a + c) || c > (a + b)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Math.round(s * 1000) / 1000;
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
