e6bda67839ab7462430deb924e2acd2b15140ed0
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-06-03 17:25:06 +0300

    task#1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,14 @@
-﻿
+﻿function parseCount(value) {
+    if(Number.isNaN(Number.parseFloat(value))) {
+        throw new Error('Невалидное значение');
+    }
+    return Number.parseFloat(value);
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
